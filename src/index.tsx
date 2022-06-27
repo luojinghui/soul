3e7c8b2b25463415f3e91c64a8f499c482a79b3e
luojinghui <luojinghui@xylink.com>
2022-06-27 12:23:04 +0800

    feat: add vconsolle

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import Router from './route';
 import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+import VConsole from 'vconsole';
 import './index.less';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const vConsole = new VConsole();
 
 root.render(
   // <React.StrictMode>
