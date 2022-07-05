279cd2d14eecdc8f11953b4f08e24fd54c8b36f3
luojinghui <luojinghui@xylink.com>
2022-07-05 21:03:14 +0800

    feat: set domain port

--- src/enum/index.ts
+++ src/enum/index.ts
@@ -1,4 +1,10 @@
 const wssProtocol = window.location.protocol === 'http:' ? 'ws://' : 'wss://';
 
-export const imServer = `${wssProtocol}${window.location.hostname}:3001`;
-export const httpServer = `${window.location.protocol}//${window.location.hostname}:3001`;
+let port = window.location.port;
+
+if (port === '3000') {
+  port = '3001';
+}
+
+export const imServer = `${wssProtocol}${window.location.hostname}:${port}`;
+export const httpServer = `${window.location.protocol}//${window.location.hostname}:${port}`;
